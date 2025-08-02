092d7817ccd8c49832597630df7a0ffdd513c83a
Pankajkumar7970 <121349902+Pankajkumar7970@users.noreply.github.com>
2025-08-02 23:10:16 +0530

    Updated QuizCard.tsx

--- src/components/QuizCard.tsx
+++ src/components/QuizCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import { Shield, DollarSign, CheckCircle } from 'lucide-react-native';
+import { Shield, DollarSign, CircleCheck as CheckCircle } from 'lucide-react-native';
 import { Quiz, getUserProgress } from '../data/quizData';
 
 interface QuizCardProps {
