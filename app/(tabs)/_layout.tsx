27eef89dc3345f1b4ba3852dbb14f7f27a83a0b2
nishantmanocha885-cmyk <nishantmanocha885@gmail.com>
2025-08-02 19:47:52 +0530

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { Home, BookOpen, FileText, MessageCircle } from 'lucide-react-native';
+import { Chrome as Home, BookOpen, FileText, MessageCircle } from 'lucide-react-native';
 
 export default function TabLayout() {
   return (
